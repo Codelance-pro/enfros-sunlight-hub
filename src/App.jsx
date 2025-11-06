b1a204882fb4d2675755b8b5861fee6b60bf1205
suriya1331 <suriyanarayanan1331@gmail.com>
2025-11-06 17:18:35 +0530

    deploy method

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/enfros-sunlight-hub">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
